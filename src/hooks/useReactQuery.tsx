82d36fa7b4909a0624c36c92584f01350ff3fa96
jihooDev <wlgn829@kakao.com>
2024-03-04 09:08:14 +0900

    feat: reactquery setting <hook으로 정의>

---
+++ src/hooks/useReactQuery.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import React, { useState } from 'react'
+
+const useReactQuery = ({ children }: React.PropsWithChildren) => {
+    const [queryClient] = useState(() => {
+        const client = new QueryClient({
+            defaultOptions: {
+                queries: {
+                    refetchOnWindowFocus: false,
+                    staleTime: 60 * 1000
+                }
+            }
+        })
+
+        return client;
+    })
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    )
+}
+
+export default useReactQuery
