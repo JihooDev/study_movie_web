74b3defaccc10fbde69f851a9aad46f323c3a288
jihooDev <wlgn829@kakao.com>
2024-05-05 11:26:20 +0900

    feat: signIn page

---
+++ src/app/(route)/(onSignIn)/home/_component/Banner.tsx
@@ -0,0 +1,84 @@
+"use client"
+
+import { getTrendingMovie } from '@/api/movie';
+import CustomButton from '@/app/_components/CustomButton';
+import LikeButton from '@/app/_components/LikeButton';
+import Loading from '@/app/_components/Loading';
+import { COLORS } from '@/assets/colors';
+import { genreFilter } from '@/menus/menu';
+import { MovieResponse } from '@/types/responseType';
+import { Box, Center, ChakraProvider, Flex, Text } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
+export default function Banner() {
+    const [randomMovieId, setRandomMovieId] = useState<number | null>(null);
+    const router = useRouter();
+    const { data, isLoading } = useQuery<MovieResponse>({
+        queryKey: ['getTrendingMovie'],
+        queryFn: getTrendingMovie,
+        enabled: !!randomMovieId,
+    })
+
+    useEffect(() => {
+        const random_0_19 = Math.floor(Math.random() * 19);
+        const id = random_0_19 === 0 ? 1 : random_0_19;
+        setRandomMovieId(id);
+    }, [])
+
+    const movie = data?.results[randomMovieId as number];
+
+
+    if (!movie) {
+        return (
+            <Center minH={450}>
+                <Loading />
+            </Center>
+        )
+    }
+
+    const onNavigateMovieDetail = () => {
+        router.push(`/detail/${movie.id}`);
+    }
+
+    return (
+        <ChakraProvider>
+            <Flex w={'full'} h={450} backgroundColor={COLORS.black} position={'relative'}>
+                {
+                    isLoading ?
+                        <Loading />
+                        :
+                        <>
+                            <img
+                                src={`https://image.tmdb.org/t/p/w1280/${data?.results[randomMovieId as number].backdrop_path}`}
+                                alt="메인 배경 이미지"
+                                style={{ filter: 'brightness(0.5)', position: 'absolute', width: '100%', height: '100%', zIndex: 0 }}
+                            />
+                            <Flex w={'full'} h={'full'} flexDirection={'column'} justifyContent={'flex-end'} zIndex={1} px={5} py={10}>
+                                <Box>
+                                    <Text color={COLORS.white} fontSize={25} fontWeight={'bold'} mb={5}>
+                                        {movie.title}
+                                    </Text>
+                                    <Text color={COLORS.white} mb={5}>
+                                        {movie.release_date.split('-')[0]} | {genreFilter.filter(value => value.type === movie.genre_ids[0])[0].title}
+                                    </Text>
+                                    <Flex>
+                                        <CustomButton onClick={onNavigateMovieDetail}>
+                                            <Text color={COLORS.white}>
+                                                자세히 보기
+                                            </Text>
+                                        </CustomButton>
+                                        <Box ml={5}>
+                                            <LikeButton id={data?.results[randomMovieId as number].id} />
+                                        </Box>
+                                    </Flex>
+                                </Box>
+                            </Flex>
+                        </>
+                }
+            </Flex>
+        </ChakraProvider>
+    )
+}
---
+++ src/app/(route)/(onSignIn)/home/_component/ListingSection.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import MovieCard from '@/app/_components/MovieCard';
+import { COLORS } from '@/assets/colors';
+import { MovieResponse } from '@/types/responseType';
+import { Box, ChakraProvider, Text } from '@chakra-ui/react';
+import { useQueryClient } from '@tanstack/react-query';
+import React, { useEffect } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+interface Props {
+    queryKey: string,
+    title: string
+}
+
+export default function ListingSection({ queryKey, title }: Props) {
+
+    const queryClient = useQueryClient();
+
+    const data: MovieResponse | undefined = queryClient.getQueryData([queryKey]);
+
+    return (
+        <ChakraProvider>
+            <Box w={'full'} px={5} py={10}>
+                <Text
+                    color={COLORS.white}
+                    fontSize={20}
+                    fontWeight={'bold'}
+                >
+                    {title}
+                </Text>
+                <Box pr={5} mt={5}>
+                    <Swiper
+                        spaceBetween={5} //슬라이드간 거리
+                        slidesPerView={3.5}
+                        loop={true}
+                    >
+                        {
+                            data?.results.map((movie, index) => (
+                                <SwiperSlide key={index}>
+                                    <MovieCard movie={movie} />
+                                </SwiperSlide>
+                            ))
+                        }
+                    </Swiper>
+                </Box>
+            </Box>
+        </ChakraProvider>
+    )
+}
